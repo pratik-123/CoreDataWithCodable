beef91165135314854c2eb1faa1ed9f4cf91bcbd
Pratik Lad <bunty@pratiks-macbook-pro.local>
2018-09-10 10:34:50 +0530

    Initial Commit

---
+++ CoreDataWithCodable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataWithCodable
+//
+//  Created by Pratik Lad on 10/09/18.
+//  Copyright © 2018 iOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
